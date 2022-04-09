e6416f44414dc441bc4e8e7d3e3e6b76ada6e710
SandeepMopare <sandeepmopare0496@gmail.com>
2022-04-09 13:17:32 +0530

    Prescription Repository Created

---
+++ src/main/java/com/app/core/repository/PrescriptionRepository.java
@@ -0,0 +1,16 @@
+package com.app.core.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.core.entity.PrescriptionEntity;
+
+@Repository
+public interface PrescriptionRepository extends JpaRepository<PrescriptionEntity, Integer> {
+
+
+
+	PrescriptionEntity findByPrecId(int precId);
+
+	
+}
